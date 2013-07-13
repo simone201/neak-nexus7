268764ca0bed162b8c0d816c9780f6375b6e3493
Theodore Ts'o <tytso@mit.edu>
2013-07-13 00:40:35 -0400

    ext4: don't allow ext4_free_blocks() to fail due to ENOMEM
    commit e7676a704ee0a1ef71a6b23760b5a8f6896cb1a1 upstream.
    
    The filesystem should not be marked inconsistent if ext4_free_blocks()
    is not able to allocate memory.  Unfortunately some callers (most
    notably ext4_truncate) don't have a way to reflect an error back up to
    the VFS.  And even if we did, most userspace applications won't deal
    with most system calls returning ENOMEM anyway.
    
    Reported-by: Nagachandra P <nagachandra@gmail.com>
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>
    [bwh: Backported to 3.2: adjust context]
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    
    Conflicts:
    	fs/ext4/mballoc.c

--- fs/ext4/mballoc.c
+++ fs/ext4/mballoc.c
@@ -4613,10 +4613,21 @@ do_more:
 		 * blocks being freed are metadata. these blocks shouldn't
 		 * be used until this transaction is committed
 		 */
+	retry:
 		new_entry = kmem_cache_alloc(ext4_free_ext_cachep, GFP_NOFS);
 		if (!new_entry) {
+<<<<<<< HEAD
 			err = -ENOMEM;
 			goto error_return;
+=======
+			/*
+			 * We use a retry loop because
+			 * ext4_free_blocks() is not allowed to fail.
+			 */
+			cond_resched();
+			congestion_wait(BLK_RW_ASYNC, HZ/50);
+			goto retry;
+>>>>>>> 5287397... ext4: don't allow ext4_free_blocks() to fail due to ENOMEM
 		}
 		new_entry->start_blk = bit;
 		new_entry->group  = block_group;

afdbc7dd9c884270fea76275675460fc286c8f9b
souljaboy11792 <fayad.chowdhury@gmail.com>
2013-08-07 14:29:08 +0600

    Revert "Revert "Revert "Add modular interface for min/max backlight contorl via sysfs. Slightly different from Metallice's implementation. Manual on XDA."""
    This reverts commit 33dadf991c5830b401c8b6b70d42ba34ca80741d.

--- arch/arm/mach-tegra/board-grouper-panel.c
+++ arch/arm/mach-tegra/board-grouper-panel.c
@@ -27,7 +27,6 @@
 #include <linux/pwm_backlight.h>
 #include <asm/atomic.h>
 #include <linux/nvhost.h>
-#include <linux/module.h>
 #include <mach/nvmap.h>
 #include <mach/irqs.h>
 #include <mach/iomap.h>
@@ -39,11 +38,6 @@
 #include "devices.h"
 #include "gpio-names.h"
 #include <mach/board-grouper-misc.h>
-
-static unsigned int min_backlight = 10;
-module_param(min_backlight, uint, 0644);
-static unsigned int max_backlight = 255;
-module_param(max_backlight, uint, 0644);
 
 /* grouper default display board pins */
 #define grouper_lvds_avdd_en		TEGRA_GPIO_PH6
@@ -157,30 +151,11 @@ static int grouper_backlight_notify(struct device *unused, int brightness)
 	brightness = (brightness * cur_sd_brightness) / 255;
 
 	/* Apply any backlight response curve */
-	if (min_backlight < 10)
-		min_backlight = 10;
-	if (max_backlight > 255)
-		max_backlight = 255;
-	if (brightness > 255) {
+	if (brightness > 255)
 		pr_info("Error: Brightness > 255!\n");
-	} else {
-	#ifdef CONFIG_BLC
-		if ((min_backlight == 0) || (max_backlight == 0)) {
-	#endif
+	else
 		brightness = bl_output[brightness];
-	#ifdef CONFIG_BLC
-		} else {
-		if ((brightness > 0) && (brightness < min_backlight) {
-			brightness = min_backlight;
-		} else if (brightness > max_backlight) {
-				brightness = max_backlight
-		} else {
-			brightness = bl_output[brightness]
-			}
-		}
-	}
-	#endif
-}
+
 	return brightness;
 }
 

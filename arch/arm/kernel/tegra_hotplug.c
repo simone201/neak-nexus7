ddf65500c570e516a3fb75a03246a555683bd1b1
souljaboy11792 <fayad.chowdhury@gmail.com>
2013-08-12 21:42:32 +0600

    Assign values to the new vars.

--- arch/arm/kernel/tegra_hotplug.c
+++ arch/arm/kernel/tegra_hotplug.c
@@ -23,11 +23,11 @@
 #define DEFAULT_THIRD_LEVEL 50
 
 #ifdef CONFIG_CUSTOM_BLVL
-static unsigned int batt_lvl_min;
+static unsigned int batt_lvl_min = 30;
 module_param(batt_lvl_min, uint, 0644);
-static unsigned int batt_lvl_med;
+static unsigned int batt_lvl_med = 60;
 module_param(batt_lvl_med, uint, 0644);
-static unsigned int batt_lvl_max;
+static unsigned int batt_lvl_max = 80;
 module_param(batt_lvl_max, uint, 0644);
 #endif 
 

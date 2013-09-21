62729f65ee6c296165cf547e4d636c83a1cb2a8b
Theodore Ts'o <tytso@mit.edu>
2013-09-21 13:58:22 -0400

    random: allow architectures to optionally define random_get_entropy()
    commit 61875f30daf60305712e25b209ef41ced2635bad upstream.
    
    Allow architectures which have a disabled get_cycles() function to
    provide a random_get_entropy() function which provides a fine-grained,
    rapidly changing counter that can be used by the /dev/random driver.
    
    For example, an architecture might have a rapidly changing register
    used to control random TLB cache eviction, or DRAM refresh that
    doesn't meet the requirements of get_cycles(), but which is good
    enough for the needs of the random driver.
    
    Signed-off-by: "Theodore Ts'o" <tytso@mit.edu>
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    
    Conflicts:
    	drivers/char/random.c

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -609,6 +609,28 @@ static void set_timer_rand_state(unsigned int irq,
 }
 #endif
 
+<<<<<<< HEAD
+=======
+/*
+ * Add device- or boot-specific data to the input and nonblocking
+ * pools to help initialize them to unique values.
+ *
+ * None of this adds any entropy, it is meant to avoid the
+ * problem of the nonblocking pool having similar initial state
+ * across largely identical devices.
+ */
+void add_device_randomness(const void *buf, unsigned int size)
+{
+	unsigned long time = random_get_entropy() ^ jiffies;
+
+	mix_pool_bytes(&input_pool, buf, size, NULL);
+	mix_pool_bytes(&input_pool, &time, sizeof(time), NULL);
+	mix_pool_bytes(&nonblocking_pool, buf, size, NULL);
+	mix_pool_bytes(&nonblocking_pool, &time, sizeof(time), NULL);
+}
+EXPORT_SYMBOL(add_device_randomness);
+
+>>>>>>> ee698d6... random: allow architectures to optionally define random_get_entropy()
 static struct timer_rand_state input_timer_state;
 
 /*
@@ -637,7 +659,7 @@ static void add_timer_randomness(struct timer_rand_state *state, unsigned num)
 		goto out;
 
 	sample.jiffies = jiffies;
-	sample.cycles = get_cycles();
+	sample.cycles = random_get_entropy();
 	sample.num = num;
 	mix_pool_bytes(&input_pool, &sample, sizeof(sample));
 
@@ -698,7 +720,23 @@ EXPORT_SYMBOL_GPL(add_input_randomness);
 
 void add_interrupt_randomness(int irq)
 {
+<<<<<<< HEAD
 	struct timer_rand_state *state;
+=======
+	struct entropy_store	*r;
+	struct fast_pool	*fast_pool = &__get_cpu_var(irq_randomness);
+	struct pt_regs		*regs = get_irq_regs();
+	unsigned long		now = jiffies;
+	__u32			input[4], cycles = random_get_entropy();
+
+	input[0] = cycles ^ jiffies;
+	input[1] = irq;
+	if (regs) {
+		__u64 ip = instruction_pointer(regs);
+		input[2] = ip;
+		input[3] = ip >> 32;
+	}
+>>>>>>> ee698d6... random: allow architectures to optionally define random_get_entropy()
 
 	state = get_timer_rand_state(irq);
 
@@ -1320,7 +1358,12 @@ unsigned int get_random_int(void)
 	__u32 *hash = get_cpu_var(get_random_int_hash);
 	unsigned int ret;
 
-	hash[0] += current->pid + jiffies + get_cycles();
+	if (arch_get_random_int(&ret))
+		return ret;
+
+	hash = get_cpu_var(get_random_int_hash);
+
+	hash[0] += current->pid + jiffies + random_get_entropy();
 	md5_transform(hash, random_int_secret);
 	ret = hash[0];
 	put_cpu_var(get_random_int_hash);

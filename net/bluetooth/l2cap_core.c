0a82d3acc63c007d0e0814863f1f4891d2ab8974
souljaboy11792 <fayad.chowdhury@googlemail.com>
2013-12-09 15:49:13 +0100

    Revert "Bluetooth: Fix invalid length check in l2cap_information_rsp()"
    This reverts commit d8f6d5b0d276bd0e8a62bf35f682484c4d117163.

--- net/bluetooth/l2cap_core.c
+++ net/bluetooth/l2cap_core.c
@@ -2805,9 +2805,6 @@ static inline int l2cap_information_rsp(struct l2cap_conn *conn, struct l2cap_cm
 	struct l2cap_info_rsp *rsp = (struct l2cap_info_rsp *) data;
 	u16 type, result;
 
-	if (cmd_len < sizeof(*rsp))
-		return -EPROTO;
-
 	type   = __le16_to_cpu(rsp->type);
 	result = __le16_to_cpu(rsp->result);
 

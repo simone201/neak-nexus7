db81cf533611521b7fec41af35e23c000d077658
souljaboy11792 <fayad.chowdhury@googlemail.com>
2013-12-10 11:15:40 +0100

    Revert "perf/arm: Fix armpmu_map_hw_event()"
    This reverts commit f646fed12abd2816b852e33f91da570c17eaa340.

--- arch/arm/kernel/perf_event.c
+++ arch/arm/kernel/perf_event.c
@@ -157,13 +157,8 @@ armpmu_map_cache_event(u64 config)
 static int
 armpmu_map_event(u64 config)
 {
-	int mapping;
-
-	if (config >= PERF_COUNT_HW_MAX)
-		return -ENOENT;
-
-	mapping = (*event_map)[config];
-	return mapping == HW_OP_UNSUPPORTED ? -ENOENT : mapping;
+	int mapping = (*armpmu->event_map)[config];
+	return mapping == HW_OP_UNSUPPORTED ? -EOPNOTSUPP : mapping;
 }
 
 static int
